0dfba2a0e613d89310e9cbf19b03f623c0a5ceb1
Qingtian Wang <wqt.net@gmail.com>
2022-11-10 23:34:25 -0600

    init

---
+++ src/main/java/elf4j/slf4j/Slf4jLogger.java
@@ -0,0 +1,269 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Easy Logging Facade for Java (ELF4J)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package elf4j.slf4j;
+
+import elf4j.Level;
+import elf4j.Logger;
+import elf4j.util.NoopLogger;
+import lombok.NonNull;
+import lombok.ToString;
+import net.jcip.annotations.Immutable;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.CallerBoundaryConfigurableLoggingEventBuilder;
+import org.slf4j.spi.LoggingEventBuilder;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
+
+import static elf4j.Level.*;
+
+@Immutable
+@ToString
+class Slf4jLogger implements Logger {
+    private static final Level DEFAULT_LEVEL = INFO;
+    private static final String EMPTY_MESSAGE = "";
+    private static final EnumMap<Level, org.slf4j.event.Level> LEVEL_MAP = setLeveMap();
+    private static final EnumMap<Level, Map<String, Slf4jLogger>> LOGGER_CACHE = initLoggerCache();
+    private static final String THIS_FQCN = Slf4jLogger.class.getName();
+    @NonNull private final String name;
+    @NonNull private final Level level;
+    @NonNull private final org.slf4j.Logger nativeLogger;
+
+    private Slf4jLogger(@NonNull String name, @NonNull Level level) {
+        this.name = name;
+        this.level = level;
+        this.nativeLogger = LoggerFactory.getLogger(name);
+    }
+
+    static Slf4jLogger instance() {
+        return getLogger(CallStack.mostRecentCallerOf(Logger.class).getClassName());
+    }
+
+    static Slf4jLogger instance(String name) {
+        return getLogger(name == null ? CallStack.mostRecentCallerOf(Logger.class).getClassName() : name);
+    }
+
+    static Slf4jLogger instance(Class<?> clazz) {
+        return getLogger(clazz == null ? CallStack.mostRecentCallerOf(Logger.class).getClassName() : clazz.getName());
+    }
+
+    private static Slf4jLogger getLogger(@NonNull String name, @NonNull Level level) {
+
+        return LOGGER_CACHE.get(level).computeIfAbsent(name, k -> new Slf4jLogger(k, level));
+    }
+
+    private static Slf4jLogger getLogger(String name) {
+        return getLogger(name, DEFAULT_LEVEL);
+    }
+
+    private static EnumMap<Level, Map<String, Slf4jLogger>> initLoggerCache() {
+        EnumMap<Level, Map<String, Slf4jLogger>> loggerCache = new EnumMap<>(Level.class);
+        EnumSet.allOf(Level.class).forEach(level -> loggerCache.put(level, new ConcurrentHashMap<>()));
+        return loggerCache;
+    }
+
+    private static EnumMap<Level, org.slf4j.event.Level> setLeveMap() {
+        EnumMap<Level, org.slf4j.event.Level> levelMap = new EnumMap<>(Level.class);
+        levelMap.put(TRACE, org.slf4j.event.Level.TRACE);
+        levelMap.put(DEBUG, org.slf4j.event.Level.DEBUG);
+        levelMap.put(INFO, org.slf4j.event.Level.INFO);
+        levelMap.put(WARN, org.slf4j.event.Level.WARN);
+        levelMap.put(ERROR, org.slf4j.event.Level.ERROR);
+        return levelMap;
+    }
+
+    @Override
+    public Logger atTrace() {
+        return atLevel(TRACE);
+    }
+
+    @Override
+    public Logger atDebug() {
+        return atLevel(DEBUG);
+    }
+
+    @Override
+    public Logger atInfo() {
+        return atLevel(INFO);
+    }
+
+    @Override
+    public Logger atWarn() {
+        return atLevel(WARN);
+    }
+
+    @Override
+    public Logger atError() {
+        return atLevel(ERROR);
+    }
+
+    @Override
+    public @NonNull String getName() {
+        return this.name;
+    }
+
+    @Override
+    public @NonNull Level getLevel() {
+        return this.level;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        if (this.level == OFF) {
+            return false;
+        }
+        return !isLevelDisabled();
+    }
+
+    @Override
+    public void log(Object message) {
+        if (isLevelDisabled()) {
+            return;
+        }
+        slf4jLog(null, Objects.toString(message), (Object[]) null);
+    }
+
+    @Override
+    public void log(Supplier<?> message) {
+        if (isLevelDisabled()) {
+            return;
+        }
+        slf4jLog(null, Objects.toString(message.get()), (Object[]) null);
+    }
+
+    @Override
+    public void log(String message, Object... args) {
+        if (isLevelDisabled()) {
+            return;
+        }
+        slf4jLog(null, message, args);
+    }
+
+    @Override
+    public void log(String message, Supplier<?>... args) {
+        if (isLevelDisabled()) {
+            return;
+        }
+        slf4jLog(null, message, Arrays.stream(args).map(Supplier::get).toArray(Object[]::new));
+    }
+
+    @Override
+    public void log(Throwable t) {
+        if (isLevelDisabled()) {
+            return;
+        }
+        slf4jLog(t, EMPTY_MESSAGE, (Object[]) null);
+    }
+
+    @Override
+    public void log(Throwable t, Object message) {
+        if (isLevelDisabled()) {
+            return;
+        }
+        slf4jLog(t, Objects.toString(message), (Object[]) null);
+    }
+
+    @Override
+    public void log(Throwable t, Supplier<?> message) {
+        if (isLevelDisabled()) {
+            return;
+        }
+        slf4jLog(t, message == null ? null : Objects.toString(message.get()), (Object[]) null);
+    }
+
+    @Override
+    public void log(Throwable t, String message, Object... args) {
+        if (isLevelDisabled()) {
+            return;
+        }
+        slf4jLog(t, message, args);
+    }
+
+    @Override
+    public void log(Throwable t, String message, Supplier<?>... args) {
+        if (isLevelDisabled()) {
+            return;
+        }
+        slf4jLog(t, message, args == null ? null : Arrays.stream(args).map(Supplier::get).toArray(Object[]::new));
+    }
+
+    private void slf4jLog(Throwable t, String message, Object... args) {
+        LoggingEventBuilder loggingEventBuilder = new CallerBoundaryConfigurableLoggingEventBuilder(nativeLogger,
+                LEVEL_MAP.get(this.level),
+                THIS_FQCN).setMessage(message).setCause(t);
+        if (args != null) {
+            for (Object arg : args) {
+                loggingEventBuilder = loggingEventBuilder.addArgument(arg);
+            }
+        }
+        loggingEventBuilder.log();
+    }
+
+    private Logger atLevel(Level level) {
+        if (this.level == level) {
+            return this;
+        }
+        return level == OFF ? NoopLogger.INSTANCE : getLogger(this.name, level);
+    }
+
+    private boolean isLevelDisabled() {
+        switch (this.level) {
+            case TRACE:
+                return !nativeLogger.isTraceEnabled();
+            case DEBUG:
+                return !nativeLogger.isDebugEnabled();
+            case INFO:
+                return !nativeLogger.isInfoEnabled();
+            case WARN:
+                return !nativeLogger.isWarnEnabled();
+            case ERROR:
+                return !nativeLogger.isErrorEnabled();
+            default:
+                return true;
+        }
+    }
+
+    private static class CallStack {
+
+        static StackTraceElement mostRecentCallerOf(@NonNull Class<?> calleeClass) {
+            StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+            String calleeClassName = calleeClass.getName();
+            for (int i = 0; i < stackTrace.length; i++) {
+                if (calleeClassName.equals(stackTrace[i].getClassName())) {
+                    for (int j = i + 1; j < stackTrace.length; j++) {
+                        if (!calleeClassName.equals(stackTrace[j].getClassName())) {
+                            return stackTrace[j];
+                        }
+                    }
+                    break;
+                }
+            }
+            throw new NoSuchElementException("unable to locate caller class of " + calleeClass + " in call stack "
+                    + Arrays.toString(stackTrace));
+        }
+    }
+}
+
---
+++ src/test/java/elf4j/slf4j/Slf4jLoggerTest.java
@@ -0,0 +1,161 @@
+package elf4j.slf4j;
+
+import elf4j.Level;
+import elf4j.Logger;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Slf4jLoggerTest {
+    public static final Logger LOGGER = Logger.instance(Slf4jLoggerTest.class);
+
+    @Nested
+    class levels {
+
+        @Test
+        void optToSupplyDefaultLevelAsInfo() {
+            assertTrue(LOGGER.atInfo().isEnabled());
+        }
+
+        @Test
+        void noArgAtHonorsLeveOnMethodName() {
+            assertFalse(LOGGER.atTrace().isEnabled());
+            assertTrue(LOGGER.atDebug().isEnabled());
+        }
+    }
+
+    @Nested
+    class log {
+
+        @Test
+        void object() {
+            LOGGER.log("log message");
+        }
+
+        @Test
+        void supplier() {
+            LOGGER.atTrace().log(() -> "supplier message");
+        }
+
+        @Test
+        void messageAndArgs() {
+            LOGGER.atDebug().log("message arg1 {}, arg2 {}", "a11111", new Object());
+        }
+
+        @Test
+        void messageAndSuppliers() {
+            LOGGER.atWarn()
+                    .log("message supplier arg1 {}, arg2 {}, arg3 {}",
+                            () -> "a11111",
+                            () -> "a22222",
+                            () -> Arrays.stream(new Object[] { "a33333" }).collect(Collectors.toList()));
+        }
+
+        @Test
+        void throwable() {
+            LOGGER.atError().log(new Exception("ex message"));
+        }
+
+        @Test
+        void throwableAndMessage() {
+            LOGGER.atError().log(new Exception("ex message"), "log message");
+        }
+
+        @Test
+        void throwableAndSupplier() {
+            LOGGER.atError().log(new Exception("ex message"), () -> "supplier log message");
+        }
+
+        @Test
+        void throwableAndMessageAndArgs() {
+            LOGGER.atError().log(new Exception("ex message"), "log message with arg {}", "a11111");
+        }
+
+        @Test
+        void throwableAndMessageAndSupplierArgs() {
+            LOGGER.atError()
+                    .log(new Exception("ex message"),
+                            "log message with supplier arg1 {}, arg2 {}, arg3 {}",
+                            () -> "a11111",
+                            () -> "a22222",
+                            () -> Arrays.stream(new Object[] { "a33333" }).collect(Collectors.toList()));
+        }
+    }
+
+    @Nested
+    class name {
+        @Test
+        void loggerNameForNullOrNoargInstanceCaller() {
+            String thisClassName = this.getClass().getName();
+            assertEquals(thisClassName, Logger.instance().getName());
+            assertEquals(thisClassName, Logger.instance((Class<?>) null).getName());
+            assertEquals(thisClassName, Logger.instance((String) null).getName());
+        }
+
+        @Test
+        void blankOrEmptyNamesStayAsIs() {
+            String blank = "   ";
+            assertEquals(blank, Logger.instance(blank).getName());
+            String empty = "";
+            assertEquals("", Logger.instance(empty).getName());
+        }
+    }
+
+    @Nested
+    class readmeSamples {
+        private final Logger logger = Logger.instance(readmeSamples.class);
+
+        @Test
+        void messagesArgsAndGuards() {
+            logger.atWarn().log("message with arguments - arg1 {}, arg2 {}, arg3 {}", "a11111", "a22222", "a33333");
+            logger.atInfo().log("info message");
+            Logger debug = logger.atDebug();
+            assertNotSame(logger, debug);
+            assertEquals(logger.getName(), debug.getName());
+            assertEquals(Level.DEBUG, debug.getLevel());
+            if (debug.isEnabled()) {
+                debug.log("a {} guarded by a {}, so {} is created {} DEBUG level is {}",
+                        "long message",
+                        "level check",
+                        "no message object",
+                        "unless",
+                        "enabled by the configuration of the logging provider");
+            }
+            debug.log(() -> "alternative to the level guard, using a supplier function should achieve the same goal, pending quality of the logging provider");
+        }
+
+        @Test
+        void throwableAndMessageAndArgs() {
+            logger.atInfo().log("let's see immutability in action...");
+            Logger error = logger.atError();
+            error.log("this is an immutable logger instance whose level is Level.ERROR");
+            Throwable ex = new Exception("ex message");
+            error.log(ex);
+            error.log(ex, "level set omitted but we know the level is Level.ERROR");
+            error.atWarn()
+                    .log(ex,
+                            "the log level switched to WARN on the fly. that is, {} returns a {} and {} Logger {}",
+                            "atWarn()",
+                            "different",
+                            "immutable",
+                            "instance");
+            error.atError()
+                    .log(ex,
+                            "here the {} call is {} because the {} instance is {}, and the instance's log level has and will always be Level.ERROR",
+                            "atError()",
+                            "unnecessary",
+                            "error logger",
+                            "immutable");
+            error.log(ex,
+                    "now at Level.ERROR, together with the exception stack trace, logging some items expensive to compute: item1 {}, item2 {}, item3 {}, item4 {}, ...",
+                    () -> "i11111",
+                    () -> "i22222",
+                    () -> "i33333",
+                    () -> Arrays.stream(new Object[] { "i44444" }).collect(Collectors.toList()));
+        }
+    }
+}
